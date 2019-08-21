c1b094b96c46aedc862afbf33c2cdda43af2067b
Hoang Quan <quanlybanthan@gmail.com>
2019-08-22 01:21:14 +0700

    setting origin api

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module BlogMe
     end
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins Rails.application.credentials[Rails.env.to_sym][:allowed_origins]
+        origins Rails.application.secrets.allowed_origins
         resource '*',
           headers: :any,
           methods: [:get, :post, :delete, :put, :patch]
